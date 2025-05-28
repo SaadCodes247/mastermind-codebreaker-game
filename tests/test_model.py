30575f47441baeaee7e262fbc178b6645ad3ff4a
Saad Ahmed <saadahmed997@gmail.com>
2025-05-28 21:10:06 +0100

    checkpoint: saving progress - controller tests work

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,18 +1,18 @@
-test model.py
+# test model.py
 
-from controller import GameController
-from model import GameModel
+# from controller import GameController
+# from model import GameModel
 
-class TestGameModel:
+# class TestGameModel:
 
-    def test_set_codemaker_code_to_break(self, monkeypatch, capsys):
-        monkeypatch.setattr("builtins.input", lambda user_entry: "1")
-        model = game_model()
-        controller = game_controller()
-        controller.begin_game()
-        printed_menu = capsys.readouterr()
-        assert  " *** Welcome to the Mastermind game! Please select a menu choice *** \n" in printed_menu
+#     def test_set_codemaker_code_to_break(self, monkeypatch, capsys):
+#         monkeypatch.setattr("builtins.input", lambda user_entry: "1")
+#         model = game_model()
+#         controller = game_controller()
+#         controller.begin_game()
+#         printed_menu = capsys.readouterr()
+#         assert  " *** Welcome to the Mastermind game! Please select a menu choice *** \n" in printed_menu
 
 
-    def test_play_player_vs_computer():
-        pass
+#     def test_play_player_vs_computer():
+#         pass
